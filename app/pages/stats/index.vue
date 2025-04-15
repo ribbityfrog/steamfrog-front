32e65ec514b627ee44c84327424bc6f4d459ca9f
LeX <pierre@lastexile.fr>
2025-04-15 21:02:48 +0200

    Removing place holder text

--- app/pages/stats/index.vue
+++ app/pages/stats/index.vue
@@ -7,18 +7,9 @@ const labels = ['Games', 'DLCs']
 <template>
     <Page>
         <Hh1>Global basics</Hh1>
-        <Section class="text-lg text-center">
-            <p>
-                If you are here, I probably gave you the link too early <br>
-                But is it my fault ? Kind of <br>
-                But who am I to judge ? <br>
-                See you tomorrow
-            </p>
-        </Section>
         <Section>
             <ClientOnly>
                 <Chart width="500" type="pie" :labels="labels" :series="series" />
-                <!-- <ApexChart width="500" :series="series" type="pie" :options="options" /> -->
             </ClientOnly>
         </Section>
     </Page>
